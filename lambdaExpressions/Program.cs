c2582bc99399576d713e4d965dcc12698c4f7b02
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-04 13:00:57 +0530

    ADDED:Welcome message

---
+++ lambdaExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Person Data Management");
+            Console.ReadKey();
+        }
+    }
+}
